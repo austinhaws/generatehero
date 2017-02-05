c8803d3eac2d2c993bb645876f2f947fd7b3dd73
Austin Haws <austinhaws@gmail.com>
2017-02-04 21:58:54 -0700

    - remove exceptions for attribute bonuses

--- test/tables/superabilities/TableSuperAbilitiesMinorTest.php
+++ test/tables/superabilities/TableSuperAbilitiesMinorTest.php
@@ -7,7 +7,7 @@ use Heroes\tables\superabilities\TableSuperAbilities;
 use Heroes\tests\utilities\TestRoll;
 use Heroes\tests\utilities\TestRoller;
 
-class TableSuperAbilitiesTest extends \PHPUnit_Framework_TestCase
+class TableSuperAbilitiesMinorTest extends \PHPUnit_Framework_TestCase
 {
     private $engine;
     private $tableSuperAbilities;
@@ -74,6 +74,66 @@ class TableSuperAbilitiesTest extends \PHPUnit_Framework_TestCase
             new TestRoll(100, 43, 'Super Ability: Minor'),
             new TestRoll(100, 47, 'Super Ability: Minor'),
             new TestRoll(100, 50, 'Super Ability: Minor'),
+        ]);
+
+        $hero = new Hero();
+        $this->tableSuperAbilities->randomSuperAbilities($hero);
+
+        $this->engine->roller->verifyTestRolls();
+    }
+
+    public function testSuperAbilities_fifthFour() {
+        $this->engine->roller->setTestRolls([
+            new TestRoll(100, 32, 'Super Abilities'),
+            new TestRoll(100, 54, 'Super Ability: Minor'),
+            new TestRoll(100, 57, 'Super Ability: Minor'),
+            new TestRoll(100, 60, 'Super Ability: Minor'),
+            new TestRoll(100, 64, 'Super Ability: Minor'),
+        ]);
+
+        $hero = new Hero();
+        $this->tableSuperAbilities->randomSuperAbilities($hero);
+
+        $this->engine->roller->verifyTestRolls();
+    }
+
+    public function testSuperAbilities_sixthFour() {
+        $this->engine->roller->setTestRolls([
+            new TestRoll(100, 32, 'Super Abilities'),
+            new TestRoll(100, 67, 'Super Ability: Minor'),
+            new TestRoll(100, 70, 'Super Ability: Minor'),
+            new TestRoll(100, 73, 'Super Ability: Minor'),
+            new TestRoll(100, 76, 'Super Ability: Minor'),
+        ]);
+
+        $hero = new Hero();
+        $this->tableSuperAbilities->randomSuperAbilities($hero);
+
+        $this->engine->roller->verifyTestRolls();
+    }
+
+    public function testSuperAbilities_seventhFour() {
+        $this->engine->roller->setTestRolls([
+            new TestRoll(100, 32, 'Super Abilities'),
+            new TestRoll(100, 80, 'Super Ability: Minor'),
+            new TestRoll(100, 84, 'Super Ability: Minor'),
+            new TestRoll(100, 87, 'Super Ability: Minor'),
+            new TestRoll(100, 90, 'Super Ability: Minor'),
+        ]);
+
+        $hero = new Hero();
+        $this->tableSuperAbilities->randomSuperAbilities($hero);
+
+        $this->engine->roller->verifyTestRolls();
+    }
+
+    public function testSuperAbilities_eightFour() {
+        $this->engine->roller->setTestRolls([
+            new TestRoll(100, 32, 'Super Abilities'),
+            new TestRoll(100, 94, 'Super Ability: Minor'),
+            new TestRoll(100, 97, 'Super Ability: Minor'),
+            new TestRoll(100, 100, 'Super Ability: Minor'),
+            new TestRoll(100, 1, 'Super Ability: Minor'),
         ]);
 
         $hero = new Hero();
