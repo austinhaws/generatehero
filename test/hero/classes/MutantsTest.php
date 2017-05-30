abbb44c6eb5c5c442fe3be2b78f4c3bf82d371fd
Austin Haws <ahaws@utah.gov>
2017-05-30 05:57:41 -0600

    Complete Mutant Animal Education Unit Test
    This test is a little squirrely. With wild education being so wild, the
    test is less predictable causing this to be a little fragile, but it's
    finally working so it is what it is.
    
    Added college/highschool skill sets.

--- test/hero/classes/MutantsTest.php
+++ test/hero/classes/MutantsTest.php
@@ -7,6 +7,8 @@ use Heroes\tests\utilities\TestRoll;
 
 class MutantsTest extends BaseTestRunner
 {
+    private $iterationOrgRelationEnded = false;
+
     public function mutant_unusual(&$rolls, $i)
     {
         return $this->iterationSubRolls($rolls, $i, [
@@ -111,5 +113,126 @@ class MutantsTest extends BaseTestRunner
                 $this->assertTrue(count($hero->psionics) > 0);
             }
         }
+    }
+
+    public function test_mutantAnimal() {
+        $ended = false;
+        for ($i = 0; !$ended; $i++) {
+            $this->testArrayTools->rotation = $i + 1;
+            $rolls = [
+                (new TestRoll())->dontCareUntil('power category')->andRoll(100, 50, 'power category'),
+                new TestRoll(2, 2, 'Mutant Type'),
+            ];
+
+            $ended = $this->runIterationGroup([
+                'animalMutantCause',
+                'animalMutantEducation',
+            ], $rolls, $i);
+
+            if ($i === 7 || $i === 8) {
+                $rolls[] = new TestRoll(3, 1, 'Mechanical Program Skills');
+            }
+
+            if ($i < 12) {
+                $ended = $this->runIterationGroup([
+                        'animalMutantOrganization',
+                    ], $rolls, $i) && $ended;
+            }
+
+            $rolls[] = new TestRoll(false, false, 'Alignment', false, true);
+
+            $this->testRoller->setTestRolls($rolls);
+
+            $hero = $this->heroGenerator->generate();
+            $this->testRoller->verifyTestRolls();
+
+            $this->assertTrue(strpos(get_class($hero->class), 'Mutants') !== false);
+        }
+    }
+
+    public function animalMutantCause(&$rolls, $i)
+    {
+        $ended = [
+            $this->iterationSubRolls($rolls, $i, [
+                // do 10 experiments so that can test all organization relationships
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Cause'),],
+
+                // wild educations
+                [new TestRoll(100, 14, 'Mutant Animal: Cause'),],
+                [new TestRoll(100, 60, 'Mutant Animal: Cause'),],
+            ])
+        ];
+
+        // experiment
+        if ($i < 10) {
+            $ended[] = $this->runIterationGroup([
+                'animalMutantOrganizationRelationship',
+            ], $rolls, $i);
+            // only gets to this test once, so only going to be able to test one of them, sorry
+            $this->iterationOrgRelationEnded = true;
+        } else {
+            $ended[] = $this->iterationOrgRelationEnded;
+        }
+        // all have to be ended in order to be ended
+        return array_reduce($ended, function ($result, $end) {
+            return $result && $end;
+        }, true);
+
+    }
+
+    public function animalMutantEducation(&$rolls, $i)
+    {
+        if ($i < 12 && ($i >= 10 || $i === 2 || $i === 4)) {
+            $ended = $this->iterationSubRolls($rolls, $i, [
+                [new TestRoll(100, 20, 'Mutant Animal: Wild Education'),],
+                [new TestRoll(100, 40, 'Mutant Animal: Wild Education'),],
+                [new TestRoll(100, 90, 'Mutant Animal: Wild Education'),],
+                [new TestRoll(100, 100, 'Mutant Animal: Wild Education'),],
+            ]);
+        } else {
+            $ended = $i >= 12;
+        }
+
+        return $ended;
+    }
+
+    public function animalMutantOrganization(&$rolls, $i)
+    {
+        return $this->iterationSubRolls($rolls, $i, [
+            [new TestRoll(100, 25, 'Mutant Animal: Organization'),],
+            [new TestRoll(100, 45, 'Mutant Animal: Organization'),],
+            [new TestRoll(100, 50, 'Mutant Animal: Organization'),],
+            [new TestRoll(100, 55, 'Mutant Animal: Organization'),],
+            [new TestRoll(100, 60, 'Mutant Animal: Organization'),],
+            [new TestRoll(100, 65, 'Mutant Animal: Organization'),],
+            [new TestRoll(100, 70, 'Mutant Animal: Organization'),],
+            [new TestRoll(100, 75, 'Mutant Animal: Organization'),],
+            [new TestRoll(100, 100, 'Mutant Animal: Organization'),],
+        ]);
+    }
+
+    public function animalMutantOrganizationRelationship(&$rolls, $i)
+    {
+        return $this->iterationSubRolls($rolls, $i, [
+            [new TestRoll(100, 10, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 20, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 30, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 40, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 50, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 60, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 70, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 80, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 90, 'Mutant Animal: organization relationship'),],
+            [new TestRoll(100, 100, 'Mutant Animal: organization relationship'),],
+        ]);
     }
 }
