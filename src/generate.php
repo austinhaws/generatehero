63a671cc7a315016427b3cb9b578b843c1b53ca4
Austin Haws <ahaws@utah.gov>
2017-12-22 07:16:37 -0700

    Generate a hero php

---
+++ src/generate.php
@@ -0,0 +1,14 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// composer autoloading
+require_once __DIR__.'/../vendor/autoload.php';
+
+
+try {
+    // generate a character and output the json
+    echo json_encode((new \Heroes\HeroGenerator())->generate());
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
