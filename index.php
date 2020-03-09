49b1ce9231b97eac61799660cc07ef1049e50827
Austin Haws <austinhaws@gmail.com>
2020-03-09 06:39:34 -0600

    Always provider CORS header

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 <?php 
 
-if($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
-    header("Access-Control-Allow-Origin: *");
-    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-    header("Content-Length: 0");
-    header("Content-Type: text/plain");
-    exit();
-}
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 
 require __DIR__ . '/src/generate.php';
