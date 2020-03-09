16418149e6000e8d2a95e4120deac4397a76daf3
Austin Haws <austinhaws@gmail.com>
2020-03-09 05:53:57 -0600

    Build composer & Provide CORS

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php 
-header("Access-Control-Allow-Origin: *");
+
+if($_SERVER['REQUEST_METHOD'] == "OPTIONS") {
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    header("Content-Length: 0");
+    header("Content-Type: text/plain");
+    exit();
+}
 
 require __DIR__ . '/src/generate.php';
